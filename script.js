3ade5b2545ec169e2204cc2f4518ab032cecf7e2
Bisha4225 <kumarbishal4225@gmail.com>
2023-01-16 20:54:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".closemodal");
+const btnsOpenModal = document.querySelectorAll(".showmodal");
+console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove(".hidden");
+  });
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add(".hidden");
+};
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  console.log(e);
+});
